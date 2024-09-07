1b537660f79fe4983bff1a1cb46bc6d9657def77
AmenZelealem <152961269+AmenZelealem@users.noreply.github.com>
2024-09-07 19:04:52 +0300

    third commit

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,4 +1,22 @@
+"use client";
 
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs
+} from 'react-icons/fa'
+
+import {SiTalwindcss, SiNextdotjs} from "react-icons/si"
+
+//about data
+
+const about={
+  title:'About me',
+  description:""//Contine from 1:34:38
+}
 
 const Resume = () => {
   return (
